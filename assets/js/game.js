bda507df3ec5bb08c9c8d4773a0a50925627da4c
Lonxyboi <drewthemachine22@gmail.com>
2022-04-13 15:35:04 -0400

    added some js

---
+++ assets/js/game.js
@@ -0,0 +1,16 @@
+// Create function named "fight"
+function fight() {
+    window.alert("The fight has begun");
+}
+//fight();
+
+var name = "your name";
+
+console.log(name);
+
+
+//defining variable playerName
+var playerName = window.prompt("What is your robot's name?");
+
+//declaring player name here
+console.log("Your robot's name is " + playerName);
